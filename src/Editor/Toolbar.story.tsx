0dc32ce3d1a87fc39b9034cb767b037844b5bb04
Shevil Mistry <shevilmistry001@gmail.com>
2025-07-24 01:51:29 +0530

    feat(): Added subscript and super script along with nodes - code block, ordered list and unordered list

--- src/Editor/Toolbar.story.tsx
+++ src/Editor/Toolbar.story.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import Subscript from '@tiptap/extension-subscript';
+import Superscript from '@tiptap/extension-superscript';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import { useEffect } from 'react';
 
 import { Toolbar } from './Toolbar.js';
 
@@ -20,12 +23,27 @@ export type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   render: () => {
     const editor = useEditor({
-      extensions: [StarterKit],
+      extensions: [StarterKit, Subscript, Superscript],
       content: '<p>Type here...</p>',
     });
 
     return (
       <div>
+        <style>
+          {`
+            .editor-container ul {
+              list-style-type: disc;
+              margin-left: 1.5rem;
+              padding-left: 1rem;
+            }
+
+            .editor-container ol {
+              list-style-type: decimal;
+              margin-left: 1.5rem;
+              padding-left: 1rem;
+            }
+          `}
+        </style>
         <Toolbar editor={editor} />
         <div
           style={{
@@ -34,7 +52,9 @@ export const Primary: Story = {
             outline: 'none',
           }}
         >
-          <EditorContent editor={editor} />
+          <div className='editor-container'>
+            <EditorContent editor={editor} />
+          </div>
         </div>
       </div>
     );
