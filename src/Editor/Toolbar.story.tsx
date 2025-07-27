843af5baec0e329fc92e8d49d39a73546ab19b6d
Shevil Mistry <shevilmistry001@gmail.com>
2025-07-28 01:12:26 +0530

    feat(): image/file import, paste in editor

--- src/Editor/Toolbar.story.tsx
+++ src/Editor/Toolbar.story.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import FileHandler from '@tiptap/extension-file-handler';
+import Image from '@tiptap/extension-image';
 import Subscript from '@tiptap/extension-subscript';
 import Superscript from '@tiptap/extension-superscript';
 import { EditorContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import { useEffect } from 'react';
 
 import { Toolbar } from './Toolbar.js';
 
@@ -23,7 +24,65 @@ export type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   render: () => {
     const editor = useEditor({
-      extensions: [StarterKit, Subscript, Superscript],
+      extensions: [
+        StarterKit,
+        Subscript,
+        Superscript,
+        Image,
+        FileHandler.configure({
+          allowedMimeTypes: [
+            'image/png',
+            'image/jpeg',
+            'image/gif',
+            'image/webp',
+          ],
+          onDrop: (currentEditor, files, pos) => {
+            files.forEach((file) => {
+              const fileReader = new FileReader();
+
+              fileReader.readAsDataURL(file);
+              fileReader.onload = () => {
+                currentEditor
+                  .chain()
+                  .insertContentAt(pos, {
+                    type: 'image',
+                    attrs: {
+                      src: fileReader.result,
+                    },
+                  })
+                  .focus()
+                  .run();
+              };
+            });
+          },
+          onPaste: (currentEditor, files, htmlContent) => {
+            files.forEach((file) => {
+              if (htmlContent) {
+                // if there is htmlContent, stop manual insertion & let other extensions handle insertion via inputRule
+                // you could extract the pasted file from this url string and upload it to a server for example
+                console.log(htmlContent); // eslint-disable-line no-console
+                return false;
+              }
+
+              const fileReader = new FileReader();
+
+              fileReader.readAsDataURL(file);
+              fileReader.onload = () => {
+                currentEditor
+                  .chain()
+                  .insertContentAt(currentEditor.state.selection.anchor, {
+                    type: 'image',
+                    attrs: {
+                      src: fileReader.result,
+                    },
+                  })
+                  .focus()
+                  .run();
+              };
+            });
+          },
+        }),
+      ],
       content: '<p>Type here...</p>',
     });
 
